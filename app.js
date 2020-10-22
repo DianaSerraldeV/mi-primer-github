a352a3d99ae7eb54799378e1e1dadd0d52104759
Diana Serralde <dianaccy95@gmail.com>
2020-10-21 20:42:19 -0500

    Mi primer commit

---
+++ app.js
@@ -0,0 +1,76 @@
+//var nameuser ="Diana"; //creacion de una variable
+//let lastname = "Serralde"; /*Creacion de una varable
+//Existe una diferencia entre estas dos declaraciones de variables */
+
+//nameuser = 'Pepe'; //Esta asigando algo nuevo en la variable
+
+//const pi = 3.1416 //Declaracion de una constante esta variable y no se puede cambiar 
+
+//let number1 = 60;
+//let number2 = 100;
+
+/*let result = number1 +  number2;
+console.log(result);*/
+
+/*let name = 'Diana';
+let lastname = 'Serralde'
+
+let completename = name + ' ' + lastname;
+console.log(completename);*/
+
+//let result = number1 <= number2;//number1 != number2; // number1 == number2;//number1 < number2;
+//console.log(result);
+/*let passworduser= 'Diana';
+let input = '123';
+
+let result = input == passworduser;
+
+if(result == true){
+    console.log('Es correcto')
+} 
+else if{
+    console.log('Es incorrecto');
+}
+else{
+
+}*/
+
+/*let typeCard = ' Debid Card';
+switch (typeCard) {
+    case 'Debid Card':
+        console.log('Debito');
+        break;
+    case 'Credit Card':
+        console.log('Credito');
+        break;
+    default:
+        console.log('Ahorro');
+}*/
+
+/*let count = 50;
+
+while(count > 0){
+    console.log(count);
+    count--;
+}*/
+
+
+/*let names = ['Diana', 'Fernanda', 'Jose'];
+console.log(names.length);
+
+for(let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}*/
+
+function saludar (namePerson){
+    console.log('Hola ' + namePerson);
+}
+
+saludar('Diana');
+
+function sumar(n1, n2){
+    console.log(n1 + n2);
+}
+
+sumar(2,3);
+sumar(100,900);
